8d32a2d6950d3c118e1a41b4df2c6755c3551038
Frankie <frankie@frankie-laptop.(none)>
2013-03-16 22:38:44 -0400

    Fix

--- server.py
+++ server.py
@@ -1,62 +1,75 @@
 #!/usr/bin/python
+import socket, sys, os
 
-import socket, sys
-
-class IonicDBClient:
+class IonicDB:
     def __init__(self):
-        self.port = int(sys.argv[2])
-        self.ip = sys.argv[1]
+        self.port = int(sys.argv[1])
         self.cmds = {
+                
                 "select":self.select,
                 "insert":self.insert,
                 "remove":self.remove,
-
+            
                 }
-    def main_loop(self):
+    def main(self):
+        s = socket.socket()
+        s.bind(('', self.port))
+        s.listen(1)
         while True:
-            try:
-                cmd = raw_input("IonicDB> ").split()
-                if cmd[0] == "exit":
-                    break
-                try:
-                    self.cmd = cmd[0]
-                    self.system = cmd[1]
-                    self.query = cmd[1:]
-                    self.query.remove(self.system)
-                    self.query = ' '.join(self.query)
-                    self.cmds[self.cmd]()
-                except Exception, error:
-                    print error
-                    print "Invalid Syntax"
-            except:
-                continue
-    def select(self):
-        sock = socket.socket()
-        sock.connect((self.ip, self.port))
-        query = """("{0}", "{1}", "{2}")""".format(self.cmd, self.system, self.query)
-        sock.send(query)
-        while True:
-            data = sock.recv(1024)
+            self.obj, conn = s.accept()
+            data = self.obj.recv(1024)
             if not data:
-                break
-            print data
-        sock.close()
+                continue
+            try:
+                data = eval(data)
+            except:
+                print 'error data'
+                continue
+            self.cmd = data[0]
+            self.system = data[1]
+            self.query = data[2]
+            try:
+                self.cmds[self.cmd]()
+            except Exception, error:
+                print error
+                pass
+    def select(self):
+        if not os.path.exists(self.system+".ion"):
+            with open(self.system+".ion", 'w') as file:
+                pass
+            self.obj.close()
+        else:
+            with open(self.system+".ion", 'rb') as file:
+                for x in file.readlines():
+                    if self.query.strip("{").strip("}") in x:
+                        self.obj.send(x)
+                self.obj.close()
     def insert(self):
-        sock = socket.socket()
-        sock.connect((self.ip, self.port))
-        query = """("{0}", "{1}", "{2}")""".format(self.cmd, self.system, self.query)
-        sock.send(query)
-        sock.close()
+        if not os.path.exists(self.system+".ion"):
+            with open(self.system+".ion", 'w') as file:
+                pass
+        with open(self.system+".ion", 'ab') as file:
+            file.write(self.query+"\n")
+        self.obj.close()
     def remove(self):
-        sock = socket.socket()
-        sock.connect((self.ip, self.port))
-        query = """("{0}", "{1}", "{2}")""".format(self.cmd, self.system, self.query)
-        sock.send(query)
-        sock.close()
+        if not os.path.exists(self.system+".ion"):
+            pass
+        else:
+            with open(self.system+".ion", 'rb') as file:
+                stuff = file.read().split("\n")
+                out = []
+                query = str(self.query).strip("{").strip("}")
+                for x in stuff:
+                    if x == '' or query in x:
+                        continue
+                    else:
+                        out.append(x)
+                with open(self.system+".ion", 'wb') as outs:
+                    for x in out:
+                        outs.write(x+"\n")
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print "Usage: ionic <host> <port>"
+if __name__ == "__main__":  
+    if len(sys.argv) < 2:
+        print 'Usage: ionic-server <port>'
         exit()
-    IonicDBClient().main_loop()
-
+    IonicDB().main()
